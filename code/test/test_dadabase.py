9a4a3a305589a0828d1e638cc5587036aa76ebe5
CDShen <164449450@qq.com>
2017-07-20 22:59:53 +0800

    上传数据库测试代码

--- code/test/test_dadabase.py
+++ code/test/test_dadabase.py
@@ -7,7 +7,7 @@ class MSSQL:
 		self.pwd = pwd
 		self.db = db
 
-	def __GetConnect(self):
+	def GetConnect(self):
 		if not self.db:
 			raise (NameError, "没有设置数据库信息")
 		self.conn = pymssql.connect(host=self.host, user=self.user, password=self.pwd, database=self.db, charset="utf8")
@@ -18,7 +18,7 @@ class MSSQL:
 			return cur
 
 	def ExecQuery(self, sql):
-		cur = self.__GetConnect()
+		cur = self.GetConnect()
 		cur.execute(sql)
 		resList = cur.fetchall()
 
@@ -27,23 +27,24 @@ class MSSQL:
 		return resList
 
 	def ExecNonQuery(self, sql):
-		cur = self.__GetConnect()
+		cur = self.GetConnect()
 		cur.execute(sql)
 		self.conn.commit()
 		self.conn.close()
 
 
-ms = MSSQL(host="10.9.8.153", user="sa", pwd="123456", db="ATCSIM_VIRTUAL")
-reslist = ms.ExecQuery("select * from airport")
-for i in reslist:
-	print (i)
+ms = MSSQL(host='192.168.0.215', user='sa', pwd='123456', db='ATCSIM_VIRTUAL')
+# reslist = ms.ExecQuery('select * from airport where aiport_code={}'.format('zhcc'))
+# reslist = ms.ExecQuery("select * from airport where aiport_code = 'zhcc'")
 
-
-
-
-
-
-
+str = "select * from airport where airport_code = {}".format("'zhcc'")
+print (str)
+cur = ms.GetConnect()
+cur.execute(str)
+reslist = cur.fetchall()
+for a in reslist:
+	print (a)
+ms.conn.close()
 
 
 
